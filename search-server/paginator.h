24f86e35e94e990ae17ddd018fedef61ebfac4ec
Dinislam Bakiev <dinislamon@mail.ru>
2022-02-09 15:14:43 +0300

    commit 1 for sprint 4

---
+++ search-server/paginator.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <iostream>
+
+template <typename It>
+class IteratorRange {
+public:
+    IteratorRange(It range_begin, It range_end) :
+        range_begin_(range_begin),
+        range_end_(range_end),
+        size_(distance(range_begin_, range_end_)) {
+
+    }
+    It begin() const {
+        return range_begin_;
+    }
+    It end() const {
+        return range_end_;
+    }
+    int size() const {
+        return size_;
+    }
+
+private:
+    It range_begin_;
+    It range_end_;
+    size_t size_;
+};
+
+
+template <typename It>
+class Paginator {
+public:
+    Paginator(It begin, It end, size_t page_size)
+    {
+        for (size_t range_size = distance(begin, end);range_size > 0;) {
+            const size_t current_page_size = min(page_size, range_size);
+            const It current_page_end = next(begin, current_page_size);
+            pages_.push_back({ begin, current_page_end });
+            range_size -= current_page_size;
+            begin = current_page_end;
+        }
+    }
+    auto begin() const {
+        return pages_.begin();
+    }
+    auto end() const {
+        return pages_.end();
+    }
+    size_t size() const {
+        return pages_.size();
+    }
+private:
+    vector<IteratorRange<It>> pages_;
+};
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
+
+template <typename It>
+ostream& operator << (ostream& output, IteratorRange<It> it) {
+    for (auto iter = it.begin(); iter != it.end(); iter++) {
+        output << *iter;
+    }
+    return output;
+}
---
+++ search-server/request_queue.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <deque>
+#include "search_server.h"
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+    // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate);
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status);
+    vector<Document> AddFindRequest(const string& raw_query);
+    int GetNoResultRequests() const;
+private:
+    struct QueryResult {
+        uint64_t timestamp;
+        int results;
+    };
+    deque<QueryResult> requests_;
+    const SearchServer& search_server_;
+    int no_results_requests_;
+    uint64_t current_time_;
+    const static int min_in_day_ = 1440;
+
+    void AddRequest(int results_num);
+};
